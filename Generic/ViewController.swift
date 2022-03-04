bed8434c772bda42afbb155e0e76e47a8bd1eb1a
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-03-04 17:27:04 +0200

    Initial Commit

---
+++ Generic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generic
+//
+//  Created by Ahmed M. Hassan on 04/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
