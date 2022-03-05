f1ab13647567aeecc74e4876599f5c254fbf385e
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-03-05 19:37:58 +0200

    Add image service to service locator

--- Generic/Classes/ServiceLocator/ServiceLocator.swift
+++ Generic/Classes/ServiceLocator/ServiceLocator.swift
@@ -10,5 +10,18 @@ import Foundation
 /// Provides global dependencies.
 ///
 class ServiceLocator {
-    
+
+    // MARK: - Private properties
+
+    /// ImageService
+    ///
+    private static var _imageService: ImageService = DefaultImageService()
+
+    // MARK: - Getters
+
+    /// Provides the access point to the image service.
+    /// - Returns: An implementation of the ImageService protocol. It defaults to DefaultImageService
+    static var imageService: ImageService {
+        return _imageService
+    }
 }
