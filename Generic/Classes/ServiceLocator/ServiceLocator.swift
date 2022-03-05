15157376819934867691f76ff1f95731c17f7889
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-03-05 17:04:23 +0200

    Add service locator

---
+++ Generic/Classes/ServiceLocator/ServiceLocator.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceLocator.swift
+//  Generic
+//
+//  Created by Ahmed M. Hassan on 05/03/2022.
+//
+
+import Foundation
+
+/// Provides global dependencies.
+///
+class ServiceLocator {
+    
+}
