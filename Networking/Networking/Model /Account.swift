d336b901cb1bdfcf35c58f4045067fa0b46b6967
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2022-03-19 01:18:20 +0200

    Add account with mapping

---
+++ Networking/Networking/Model /Account.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+/// Account
+///
+public struct Account: Decodable, Equatable {
+
+    /// Dotcom UserID
+    ///
+    public let userID: Int64
+
+    /// Display Name
+    ///
+    public let displayName: String
+
+    /// Account's Email
+    ///
+    public let email: String
+
+    /// Account's Username
+    ///
+    public let username: String
+
+    /// Account's Gravatar
+    ///
+    public let gravatarUrl: String?
+
+
+    /// Designated Initializer.
+    ///
+    public init(userID: Int64, displayName: String, email: String, username: String, gravatarUrl: String?) {
+        self.userID = userID
+        self.displayName = displayName
+        self.email = email
+        self.username = username
+        self.gravatarUrl = gravatarUrl
+    }
+}
+
+
+/// Defines all of the Account CodingKeys
+///
+private extension Account {
+
+    enum CodingKeys: String, CodingKey {
+        case userID         = "ID"
+        case displayName    = "display_name"
+        case email          = "email"
+        case username       = "username"
+        case gravatarUrl    = "avatar_URL"
+    }
+}
